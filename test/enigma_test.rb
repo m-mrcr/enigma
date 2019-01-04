d9514f93c50e1a4315426b45e0c2053acfd9f097
Martin Mercer <martinmercer1@gmail.com>
2019-01-04 10:28:15 -0700

    test Enigma exists

---
+++ test/enigma_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'date'
+require './lib/enigma'
+
+class EnigmaTest < MiniTest::Test
+  def test_it_exists
+    enigma = Enigma.new
+
+    assert_instance_of Enigma, enigma
+  end
+
+end
