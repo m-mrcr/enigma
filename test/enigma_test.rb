feb45247e8c5a470f052e7eb8f00b925ed326bd9
Martin Mercer <martinmercer1@gmail.com>
2019-01-05 16:39:24 -0700

    added enigma class functionality

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,3 +1,6 @@
+require 'simplecov'
+SimpleCov.start
+
 require 'minitest/autorun'
 require 'minitest/pride'
 require 'date'
@@ -8,6 +11,17 @@ class EnigmaTest < MiniTest::Test
     enigma = Enigma.new
 
     assert_instance_of Enigma, enigma
+  end
+
+  def test_it_can_encrypt_a_message
+    enigma = Enigma.new
+
+    expected = { encryption: "keder ohulw",
+                 key: "02715",
+                 date: "040895"
+               }
+
+    assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
   end
 
 end
