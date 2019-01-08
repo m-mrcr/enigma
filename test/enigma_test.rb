76aa6df594dfd58de39eaeb220e5b413addff41c
Martin Mercer <martinmercer1@gmail.com>
2019-01-08 15:33:42 -0700

    adds keys_test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -19,6 +19,12 @@ class EnigmaTest < MiniTest::Test
     assert_equal expected, enigma.character_set
   end
 
+  def test_that_it_initiates_with_empty_keys
+    enigma = Enigma.new
+    expected = {A: 0, B: 0, C: 0, D: 0}
+    assert_equal expected, enigma.keys
+  end
+
   def test_it_can_encrypt_a_message
     skip
     enigma = Enigma.new
