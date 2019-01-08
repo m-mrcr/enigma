c3533d616a67066c895167814fb26d956ed98d7b
Martin Mercer <martinmercer1@gmail.com>
2019-01-08 15:43:24 -0700

    adds offsets functionality

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,11 +1,13 @@
 class Enigma
 
   attr_reader :character_set,
-              :keys
+              :keys,
+              :offsets
 
   def initialize
     @character_set = ("a".."z").to_a << " "
     @keys = {A: 0, B: 0, C: 0, D: 0}
+    @offsets = {A: 0, B: 0, C: 0, D: 0}
   end
 
 end
