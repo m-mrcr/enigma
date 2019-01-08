5b9ac273416f00fd081a3c693ebc9e2b8c72a126
Martin Mercer <martinmercer1@gmail.com>
2019-01-08 15:34:01 -0700

    adds keys_functionality

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,9 +1,11 @@
 class Enigma
 
   attr_reader :character_set
+              :keys
 
   def initialize
     @character_set = ("a".."z").to_a << " "
+    @keys = {A: 0, B: 0, C: 0, D: 0}
   end
 
 end
