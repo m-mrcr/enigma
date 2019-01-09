3cf67ff736774e3ccd4529c95cd8668f216597e2
Martin Mercer <martinmercer1@gmail.com>
2019-01-08 23:00:59 -0700

    adds test for offsets

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,4 @@
+require 'date'
 class Enigma
 
   attr_reader :character_set,
@@ -10,5 +11,12 @@ class Enigma
     @offsets = {A: 0, B: 0, C: 0, D: 0}
   end
 
+  def generate_keys(number)
+  number = number.to_s.split('')
+    keys.each do |letter, num|
+      keys[letter] = number.slice(0..1).join.to_i
+      number.shift
+    end
+  end
 
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -8,7 +8,6 @@ require './lib/enigma'
 
 class EnigmaTest < MiniTest::Test
   def test_it_exists
-    skip
     enigma = Enigma.new
 
     assert_instance_of Enigma, enigma
@@ -16,29 +15,41 @@ class EnigmaTest < MiniTest::Test
   end
 
   def test_that_it_initiates_with_a_character_set
-    skip
     enigma = Enigma.new
     expected = ("a".."z").to_a << " "
     assert_equal expected, enigma.character_set
   end
 
   def test_that_it_initiates_with_empty_keys
-    skip
     enigma = Enigma.new
     expected = {A: 0, B: 0, C: 0, D: 0}
     assert_equal expected, enigma.keys
   end
 
   def test_that_it_initiates_with_empty_offsets
-    skip
     enigma = Enigma.new
     expected = {A: 0, B: 0, C: 0, D: 0}
     assert_equal expected, enigma.offsets
   end
 
+  def test_that_it_can_generate_keys
+    enigma = Enigma.new
+
+    expected = {A: 87, B: 75, C: 52, D: 22}
+
+    assert_equal expected, enigma.generate_keys("87522")
+  end
+
+  def test_that_it_can_generate_offsets
+    enigma = Enigma.new
+
+    expected = ["8", "4", "0", "0"]
+
+    assert_equal expected, enigma.generate_offsets("121220")
+  end
+
 
   # def test_it_can_encrypt_a_message
-  #   skip
   #   enigma = Enigma.new
   #
   #   expected = { encryption: "keder ohulw",
