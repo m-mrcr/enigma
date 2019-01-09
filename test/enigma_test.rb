dcd91b61d2e046a15433cc854bdb682d203ecfdb
Martin Mercer <martinmercer1@gmail.com>
2019-01-09 08:08:57 -0700

    add decrypt functionality

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -76,4 +76,14 @@ class EnigmaTest < MiniTest::Test
     assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
   end
 
+  def test_it_can_decrypt_a_message
+    enigma = Enigma.new
+
+    expected = { encryption: "hello world",
+                 key: "02715",
+                 date: "040895"
+               }
+    assert_equal expected, enigma.decrypt("keder ohulw", "02715", "040895")
+  end
+
 end
